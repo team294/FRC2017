93b58be4024aeec818b64ead1c37b7a5716d7044
Don Sawdai <team294@test.com>
2017-01-22 14:46:30 -0800

    TurnToAngle
    Added calculateSpeed, which takes an angle and calculates the
    appropriate speed based on that.

--- src/org/usfirst/frc/team294/robot/commands/TurnToAngle.java
+++ src/org/usfirst/frc/team294/robot/commands/TurnToAngle.java
@@ -9,29 +9,46 @@ import edu.wpi.first.wpilibj.command.Command;
 /**
  *
  */
-public class DriveToAngle extends Command {
+public class TurnToAngle extends Command {
 
+	private double angle;
 	private double speed;
-	private double curve;
+	final private double MIN_POWER = 0.2;
+	final private double MAX_POWER = 0.7;
 	
-    public DriveToAngle(double speed, double curve) {
+    public TurnToAngle(double angle) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.driveTrain);
-    	this.speed = speed;
-    	this.curve = curve;
+    	this.angle = angle;
+    	this.speed = 0;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	if (speed > 1.0) speed = 1.0;
-    	if (speed < -1.0) speed = -1.0;
-    	Robot.driveTrain.driveAtAngle(speed, curve);
+    	if (speed >= 1.0) speed = 1.0;
+    	if (speed <= -1.0) speed = -1.0;
     	
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    }
+    
+    private double calculateSpeed(double angle) {
+    	double power = 0;
+    	if (angle >= 90) {
+    		power = MAX_POWER;
+    	} else {
+    		power = MIN_POWER + angle*(MAX_POWER-MIN_POWER)/90;
+    	}
+    	if (angle <= 0) {
+    		power = MIN_POWER*-1;
+    	} else {
+    		power = MAX_POWER*-1;
+    	}
+    	return power;
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
