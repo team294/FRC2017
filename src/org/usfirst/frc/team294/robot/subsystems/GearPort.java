45868a8bc9515f52966d266c04592ad123fb423d
Dark General of Programming <x@bcr.org>
2017-01-22 17:47:41 -0800

    Added Solenoid to GearPort subclass

--- src/org/usfirst/frc/team294/robot/subsystems/GearPort.java
+++ src/org/usfirst/frc/team294/robot/subsystems/GearPort.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team294.robot.subsystems;
 
+import org.usfirst.frc.team294.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -7,8 +10,21 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class GearPort extends Subsystem {
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+	private final DoubleSolenoid gearPiston = new DoubleSolenoid(RobotMap.gearPistonOut, RobotMap.gearPistonIn);
+
+	/**
+	 * Set the gear piston to out
+	 */
+	public void out() {
+		gearPiston.set(DoubleSolenoid.Value.kForward);
+	}
+	
+	/**
+	 * Set the gear piston to in
+	 */
+	public void in() {
+		gearPiston.set(DoubleSolenoid.Value.kReverse);
+	}
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
