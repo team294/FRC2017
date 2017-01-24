3f591758f272e1a0d0fed738f9cf486358e51a77
Andrew Graven <andrew@graven.com>
2017-01-23 20:19:30 -0800

    Fixed angle offset calculations to be more failure resistant and more discriminatory, as well as more modular (in terms of the camera)

--- src/org/usfirst/frc/team294/robot/subsystems/Vision.java
+++ src/org/usfirst/frc/team294/robot/subsystems/Vision.java
@@ -13,9 +13,13 @@ public class Vision extends Subsystem {
 	
 	double[] centerX, centerY, centerGoal, height;
 	double gearAngleOffset, distance;
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-
+	
+	double camPXWidth = 240, camPXHeight = 320, camDiagonalAngle = 68.5;
+	double camPXDiagonal = Math.sqrt(camPXWidth * camPXWidth + camPXHeight * camPXHeight);
+	double camVertAngle = camPXHeight / camPXDiagonal * camDiagonalAngle;
+	double camHorizAngle = camPXWidth / camPXDiagonal * camDiagonalAngle;
+	
+	
    public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
@@ -25,12 +29,23 @@ public class Vision extends Subsystem {
 		grip_table = NetworkTable.getTable("GRIP");
 	}
    public double getGearAngleOffset() {
-	   centerX = table.getNumberArray("centerX", networkTableDefault );
-	   gearAngleOffset = .085625*(320-(centerX[0] + centerX[1])/2); //Degrees
+	   double[] areas = table.getNumberArray("area", networkTableDefault );
+	   double[] cX = table.getNumberArray("centerX", networkTableDefault );
+	   if (cX.length < 2) { return gearAngleOffset = 0; } //Break out if not enough objects detected
+	   double[] maxTwoAreas = {0, 0}; // {size1, size2}
+	   int index1 = 0, index2 = 0;
+	   for (int i = 0; i < areas.length; i++) {
+		   if (areas[i] > maxTwoAreas[0]) {maxTwoAreas[0] = areas[i]; index1 = i;}
+		   else if (areas[i] > maxTwoAreas[1]) {maxTwoAreas[1] = areas[i]; index2 = i;}
+	   }
+	   gearAngleOffset = (camPXWidth/2 - (cX[index1] + cX[index2])/2)/camPXWidth * camHorizAngle;
 	   return gearAngleOffset;
    }
    public double getDistance() {
 	   height = table.getNumberArray("height", networkTableDefault );
+	   
+	   
+	   
 	   //distance = 2.5/Math.tan(.0428125*((height[2]+height[3])/2)); //in inches
 	   distance = 2.5/Math.tan(.00037360954*(height[2]+height[3])); //in inches (faster)
 	   return distance;
