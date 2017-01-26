f557f65440bc957cb69a3e269915982ec85b59a9
Andrew Graven <andrew@graven.com>
2017-01-25 19:53:42 -0800

    Added overlap filtering to contour discrimination

--- src/org/usfirst/frc/team294/robot/subsystems/Vision.java
+++ src/org/usfirst/frc/team294/robot/subsystems/Vision.java
@@ -30,9 +30,23 @@ public class Vision extends Subsystem {
 	}
    public int[] filterContours() {
 	   //Determines the indices which two contours are most likely to be the targets
-	   double[][] contours= {table.getNumberArray("centerX", networkTableDefault ), 
+	   double[][] rawContours= {table.getNumberArray("centerX", networkTableDefault ), 
 			   				 table.getNumberArray("centerY", networkTableDefault ),
 			   				 table.getNumberArray("area", networkTableDefault )};
+	   //1. Find overlapping contours and keep larger of the two
+	   for (int i=0; i < rawContours[0].length; i++) {rawContours[3][i]  = Math.sqrt(rawContours[2][i])/2;} //Gets square x/y radius from point.
+	   double[][] contours = null;
+	   for (int a = 0; a < rawContours[0].length; a++) {
+		   for (int b = 0; b < rawContours[0].length; b++) {
+			   if (b == a) {continue; }
+			   double[] aC = {rawContours[0][a], rawContours[1][a], rawContours[2][a], rawContours[3][a]};
+			   double[] bC = {rawContours[0][b], rawContours[1][b], rawContours[2][b], rawContours[3][b]};
+			   if (aC[3] + bC[3] > Math.abs(aC[0] - bC[0]) && aC[3] + bC[3] > Math.abs(aC[1] - bC[1])) { //Do the rectangles intersect?
+				   if (aC[2] > bC[2]) {for (int i = 0; i < 4; i++ ) {contours[i][contours[0].length] = aC[i]; break;}} //If a has more area than b, add it to filtered list
+			   } else {for (int i = 0; i < 4; i++ ) {contours[i][contours[0].length] = aC[i];}} //if a does not overlap, add it to filtered list
+		   }
+	   }
+	   //2. Choose two biggest remaining contours
 	   double[] maxTwoAreas = {0, 0}; // {size1, size2}
 	   int[] index = {0, 0}; //Array for two best index values 
 	   for (int i = 0; i < contours[2].length; i++) { //Loop through each area value to determine best contours
