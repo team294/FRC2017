ea50a970e37453fc8c618ce293f6248184db6f9f
Team294 <team294@nowhere.edu>
2017-02-05 16:17:41 -0800

    added DriveToBoiler
    also uncommented left and right motor 3

---
+++ src/org/usfirst/frc/team294/robot/commands/DriveToBoiler.java
@@ -0,0 +1,49 @@
+package org.usfirst.frc.team294.robot.commands;
+
+import org.usfirst.frc.team294.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveToBoiler extends Command {
+
+    public DriveToBoiler() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.driveTrain);
+    	requires(Robot.boilerVision);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	double distance = Robot.boilerVision.getBoilerDistance();
+    	if (distance > 2) {
+    		Robot.driveTrain.driveForward(.25);
+    	}
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+       // return false;
+        if (Robot.boilerVision.getBoilerDistance() <= 2) {
+        	return true;
+        } else {
+        	return false;
+        }
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
