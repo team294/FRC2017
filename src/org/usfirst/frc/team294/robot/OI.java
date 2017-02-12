9431e1729e8a24f5a4cdb806ea419ab8e07df8ae
MarleyEdmonds <BCR@x.org>
2017-02-11 17:08:20 -0800

    Added Manual Climb Control
    Using XBox controller while B is pressed and controlled with left analog
    stick.

--- src/org/usfirst/frc/team294/robot/OI.java
+++ src/org/usfirst/frc/team294/robot/OI.java
@@ -16,6 +16,9 @@ public class OI {//Hopefully this works or maybe this will
 	// Joysticks
 	public Joystick leftJoystick = new Joystick(0);
 	public Joystick rightJoystick = new Joystick(1);
+	public Joystick coPanel = new Joystick(2);
+	public Joystick xboxController = new Joystick(3);
+	
 
 	// There are a few additional built in buttons you can use. Additionally,
 	// by subclassing Button you can create custom triggers and bind those to
@@ -39,8 +42,12 @@ public class OI {//Hopefully this works or maybe this will
 	public OI() {
 		Button[] left = new Button[12];
 	    Button[] right = new Button[12];
+	    Button[] coP = new Button[15];
+	    Button[] xbB = new Button[11];
 	    int j = 1;
 	    int k = 1;
+	    int l = 1;
+	    int m = 1;
 	    
 	    // Declare left joystick buttons and set them to shift down
 	     for (Button i : left) {
@@ -58,7 +65,16 @@ public class OI {//Hopefully this works or maybe this will
 	    	 if (k > 2 && k < 7) i.whenPressed(new DriveWithJoysticks()); 
 	    	 else i.whenPressed(new ShiftUp());
 	     }
+	     
+	     for (Button i : coP) {
+	    	 i = new JoystickButton(coPanel, l++);
+	     }
 
+	     for (Button i : xbB) {
+	    	 i = new JoystickButton(xboxController, m++);
+	    	 if (m == 2) i.whileHeld(new ClimbJoystickControl());
+	     }
+	     
 	     // Gyro Testing Commands
 	     SmartDashboard.putData("Turn to 90", new GyroTurnToAngle(0.4, 90));
 	     SmartDashboard.putData("Turn to -90", new GyroTurnToAngle(0.4, -90));
