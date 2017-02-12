4b0d2d1544a8bc91d5dad71ce16bc10946e936ec
Dark General of Programming <x@bcr.org>
2017-02-12 15:06:45 -0800

    Removed unused robot imports

--- src/org/usfirst/frc/team294/robot/Robot.java
+++ src/org/usfirst/frc/team294/robot/Robot.java
@@ -1,13 +1,10 @@
 package org.usfirst.frc.team294.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import org.usfirst.frc.team294.robot.commands.*;
 import org.usfirst.frc.team294.robot.subsystems.*;
 import org.usfirst.frc.team294.utilities.FileLog;
 
