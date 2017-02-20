42d04ee1957a7d1d05d2faad560b6512db739c52
Rob Coe <robcoe18@gmail.com>
2017-02-19 21:17:55 -0800

    general updates
    updated map, reduced intake speed,

--- src/org/usfirst/frc/team294/robot/commands/IntakeSetToSpeed.java
+++ src/org/usfirst/frc/team294/robot/commands/IntakeSetToSpeed.java
@@ -34,15 +34,18 @@ public class IntakeSetToSpeed extends Command {
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		return true;
+		return false;
 	}
 
 	// Called once after isFinished returns true
 	protected void end() {
+		Robot.intake.setSpeed(0);
 	}
 
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
 	protected void interrupted() {
+		Robot.intake.setSpeed(0);
+
 	}
 }
--- src/org/usfirst/frc/team294/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team294/robot/subsystems/Shooter.java
@@ -73,7 +73,7 @@ public class Shooter extends Subsystem {
 	public void periodicSetF(){
 		double currentBatteryVoltage = shooterMotor1.getBusVoltage();
 		double f = ((12.1/currentBatteryVoltage)*Robot.shooterFNominal);
-		f = .999 * fLast + .001 * f;
+		f = .99 * fLast + .01 * f;
 		fLast = f;
 		shooterMotor1.setF(f);
 	}
@@ -130,7 +130,7 @@ public class Shooter extends Subsystem {
 		SmartDashboard.putNumber("Shooter Motor 1000*P", shooterMotor1.getP() * 1000);
 		SmartDashboard.putNumber("Shooter Motor 1000*I", shooterMotor1.getI() * 1000);
 		SmartDashboard.putNumber("Shooter Motor 1000*D", shooterMotor1.getD() * 1000);
-		SmartDashboard.putNumber("Shooter Motor Set RPM", shooterMotor1.get());
+		SmartDashboard.putNumber("Shooter Motor Set RPM", 4000);		// this should come from preferences
 		SmartDashboard.putNumber("Shooter Motor Set Voltage", 5); 
 		SmartDashboard.putNumber("Set Nominal 1000* F Value", fNominal*1000);  
 	}
