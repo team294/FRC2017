e4e1d142f3be4a7f49fdabba696ec62a4e9c6938
Team294 <team294@nowhere.edu>
2017-02-24 20:43:36 -0800

    Fixed Merge with Maser Conflicts in Intake

--- src/org/usfirst/frc/team294/robot/subsystems/Intake.java
+++ src/org/usfirst/frc/team294/robot/subsystems/Intake.java
@@ -26,7 +26,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Intake extends Subsystem {
 
 	// Motors
-<<<<<<< HEAD
     private final CANTalon intakeMotor = new CANTalon(RobotMap.intakeMotor);
     private final CANTalon climbMotor1 = new CANTalon(RobotMap.climbMotor1);
     private final CANTalon climbMotor2 = new CANTalon(RobotMap.climbMotor2);
@@ -34,11 +33,11 @@ public class Intake extends Subsystem {
     // Pneumatics
     private final DoubleSolenoid intakeSolenoid = new DoubleSolenoid(RobotMap.intakeSolenoidFwd, RobotMap.intakeSolenoidRev);
     private final DoubleSolenoid hopperSolenoid = new DoubleSolenoid(RobotMap.hopperSolenoidFwd, RobotMap.hopperSolenoidRev);
-   
-    //Current Protection
-    public final MotorCurrentTrigger intakeCurrentTrigger = new MotorCurrentTrigger(intakeMotor, 10, 2);
-    public final MotorCurrentTrigger climber1CurrentTrigger = new MotorCurrentTrigger(climbMotor1, 35, 2);
-    public final MotorCurrentTrigger climber2CurrentTrigger = new MotorCurrentTrigger(climbMotor2, 35, 2);
+    
+	//Current Protection
+	public final MotorCurrentTrigger intakeCurrentTrigger = new MotorCurrentTrigger(intakeMotor, 7, 3);
+	List<CANTalon> climbMotors = new ArrayList<CANTalon>(Arrays.asList(climbMotor1, climbMotor2));
+	public final MotorGroupCurrentTrigger climbCurrentTrigger = new MotorGroupCurrentTrigger(climbMotors, 35, 2);
 
     // Control variables for mechanical interlock
     public static enum Status {
@@ -59,40 +58,9 @@ public class Intake extends Subsystem {
     	
     	// Set up subsystem components
     	intakeMotor.setVoltageRampRate(50);
-=======
-	private final CANTalon intakeMotor = new CANTalon(RobotMap.intakeMotor);
-	private final CANTalon climbMotor1 = new CANTalon(RobotMap.climbMotor1);
-	private final CANTalon climbMotor2 = new CANTalon(RobotMap.climbMotor2);
-
-	// Pneumatics
-	private final DoubleSolenoid intakeSolenoid = new DoubleSolenoid(RobotMap.intakeSolenoidFwd, RobotMap.intakeSolenoidRev);
-	private final DoubleSolenoid hopperSolenoid = new DoubleSolenoid(RobotMap.hopperSolenoidFwd, RobotMap.hopperSolenoidRev);
-
-	//Current Protection
-	public final MotorCurrentTrigger intakeCurrentTrigger = new MotorCurrentTrigger(intakeMotor, 7, 3);
-	List<CANTalon> climbMotors = new ArrayList<CANTalon>(Arrays.asList(climbMotor1, climbMotor2));
-	public final MotorGroupCurrentTrigger climbCurrentTrigger = new MotorGroupCurrentTrigger(climbMotors, 35, 2);
-
-	// Control variables for mechanical interlock
-	public static enum Positions {
-		deployed,stowed,unknown
-	}
-
-	private Positions intakePos = Positions.unknown;
-	private Positions hopperPos = Positions.unknown;
-
-	// Time to move hopper/intake in seconds (refine by testing)
-	public final double HOPPER_DELAY = 4.0;
-	public final double INTAKE_DELAY = 4.0;
-
-	public Intake() {
-
-		// Call the Subsystem constructor
-		super();
 
 		// Set up subsystem components
 		intakeMotor.setVoltageRampRate(50);
->>>>>>> refs/remotes/origin/master
 		climbMotor2.changeControlMode(TalonControlMode.Follower);
 		climbMotor2.set(climbMotor1.getDeviceID());
 
@@ -113,16 +81,6 @@ public class Intake extends Subsystem {
 	}
 
 	/**
-	 * Get the current position of the intake
-	 * @return true if the intake is deployed, false if not
-	 */
-	public boolean getPosition(){
-		return (intakeSolenoid.get() == DoubleSolenoid.Value.kForward);
-
-		//        updateConflicts();
-	}
-
-	/**
 	 * Set the speed of the intake motor
 	 * @param speed of the motor, between -1 (outtake) and +1 (intake), 0 = stopped
 	 */
@@ -137,6 +95,18 @@ public class Intake extends Subsystem {
 	public double getSpeed() {
 		return -intakeMotor.get();
 	}
+	
+    /**
+     * Set the speed of the climbing mechanism
+     * @param speed from 0 to +1. <b>Climber does not go down!</b>
+     */
+    public void setClimbSpeed(double speed) {
+    	climbMotor1.changeControlMode(TalonControlMode.PercentVbus);
+    	// Need to check if hopper and intake are stowed first
+    	if (speed < 0) speed = 0;
+    	if (speed > 1.0) speed = 1.0;
+    	climbMotor1.set(speed);
+    }
 
 	/**
 	 * Set up the intake controls on the SmartDashboard.  Call this once when the robot is 
@@ -149,39 +119,10 @@ public class Intake extends Subsystem {
 	/**
 	 * Send intake status to SmartDashboard
 	 */
-<<<<<<< HEAD
     public void updateSmartDashboard() {
  		SmartDashboard.putNumber("Intake motor setpoint", -intakeMotor.get());
  		SmartDashboard.putNumber("Intake motor current", intakeMotor.getOutputCurrent());
 // 		SmartDashboard.putString("Intake position", intakeIsUp() ? "Up" : "Down");
-    }
-    
-     /**
-      * Deploy the intake
-      */
-    public void deployIntake() {
-    	intakeSolenoid.set(DoubleSolenoid.Value.kForward);
-    }
-    
-    /**
-     * Stows the intake <b>only if the hopper is stowed</b>
-     */
-    public void stowIntake() {
-    	intakeSolenoid.set(DoubleSolenoid.Value.kReverse);
-    }
-    
-    /**
-     * Deploy the hopper out <b>only if the intake is deployed</b>
-     */
-    public void deployHopper() {
-    	hopperSolenoid.set(DoubleSolenoid.Value.kForward);
-    }
-    
-    /**
-     * Stow the hopper (for climbing) <b>only if the intake is deployed</b>
-     */
-    public void stowHopper() {
-    	hopperSolenoid.set(DoubleSolenoid.Value.kReverse);
     }
     
     /**
@@ -228,29 +169,6 @@ public class Intake extends Subsystem {
     public Status getIntakeTracker() {
     	return intakePos;
     }
-    
-    /**
-     * Set the speed of the climbing mechanism
-     * @param speed from 0 to +1. <b>Climber does not go down!</b>
-     */
-    public void setClimbSpeed(double speed) {
-    	climbMotor1.changeControlMode(TalonControlMode.PercentVbus);
-    	// Need to check if hopper and intake are stowed first
-    	if (speed < 0) speed = 0;
-    	if (speed > 1.0) speed = 1.0;
-    	climbMotor1.set(speed);
-    }
-    
-    /**
-     * Stops the intake rollers
-     * <p> <b>Does not change control mode</b>
-     */
-=======
-	public void updateSmartDashboard() {
-		SmartDashboard.putNumber("Intake motor setpoint", -intakeMotor.get());
-		SmartDashboard.putNumber("Intake motor current", intakeMotor.getOutputCurrent());
-		// 		SmartDashboard.putString("Intake position", intakeIsUp() ? "Up" : "Down");
-	}
 
 	/**
 	 * Deploy the intake
@@ -263,85 +181,28 @@ public class Intake extends Subsystem {
 	 * Stows the intake <b>only if the hopper is stowed</b>
 	 */
 	public void stowIntake() {
-		if (hopperPos == Positions.stowed) intakeSolenoid.set(DoubleSolenoid.Value.kReverse);
+		intakeSolenoid.set(DoubleSolenoid.Value.kReverse);
 	}
 
 	/**
 	 * Deploy the hopper out <b>only if the intake is deployed</b>
 	 */
 	public void deployHopper() {
-		if (intakePos == Positions.deployed) hopperSolenoid.set(DoubleSolenoid.Value.kForward);
+		hopperSolenoid.set(DoubleSolenoid.Value.kForward);
 	}
 
 	/**
 	 * Stow the hopper (for climbing) <b>only if the intake is deployed</b>
 	 */
 	public void stowHopper() {
-		if (intakePos == Positions.deployed) hopperSolenoid.set(DoubleSolenoid.Value.kReverse);
+		hopperSolenoid.set(DoubleSolenoid.Value.kReverse);
 	}
 
 	/**
 	 * Updates the conflicts between the hopper and the intake
 	 * Call before enabling the robot to avoid mechanical interlock
 	 */
-	public void updateConflicts() {
-		if (intakeSolenoid.get() == DoubleSolenoid.Value.kForward) intakePos = Positions.deployed;
-		else if (intakeSolenoid.get() == DoubleSolenoid.Value.kReverse) intakePos = Positions.stowed;
-		else intakePos = Positions.unknown;
-		if (hopperSolenoid.get() == DoubleSolenoid.Value.kForward) hopperPos = Positions.deployed;
-		else if (hopperSolenoid.get() == DoubleSolenoid.Value.kReverse) hopperPos = Positions.stowed;
-		else hopperPos = Positions.unknown;
-	}
 
-	/**
-	 * Set the value of the hopper tracker in the code
-	 * @param position Intake.Positions.stowed,deployed,unknown
-	 */
-	public void setHopperTracker(Positions position) {
-		hopperPos = position;
-	}
-
-	/**
-	 * Set the value of the intake tracker in the code
-	 * @param position Intake.Positions.stowed,deployed,unknown
-	 */
-	public void setIntakeTracker(Positions position) {
-		intakePos = position;
-	}
-
-	/**
-	 * Read the value of the software hopper tracker
-	 * @return Intake.Positions.deployed,stowed,unknown
-	 */
-	public Positions getHopperTracker() {
-		return hopperPos;
-	}
-
-	/**
-	 * Read the value of the software intake tracker
-	 * @return Intake.Positions.deployed,stowed,unknown
-	 */
-	public Positions getIntakeTracker() {
-		return intakePos;
-	}
-
-	/**
-	 * Set the speed of the climbing mechanism
-	 * @param speed from 0 to +1. <b>Climber does not go down!</b>
-	 */
-	public void setClimbSpeed(double speed) {
-		climbMotor1.changeControlMode(TalonControlMode.PercentVbus);
-		// Need to check if hopper and intake are stowed first
-		if (speed < 0) speed = 0;
-		if (speed > 1.0) speed = 1.0;
-		climbMotor1.set(speed);
-	}
-
-	/**
-	 * Stops the intake rollers
-	 * <p> <b>Does not change control mode</b>
-	 */
->>>>>>> refs/remotes/origin/master
 	public void stopIntake() {
 		intakeMotor.set(0.0);
 	}
