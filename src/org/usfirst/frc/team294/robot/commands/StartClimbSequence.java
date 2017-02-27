1cf8dd527e3e72580f51776aca2b673fa9123279
Rob Coe <robcoe18@gmail.com>
2017-02-27 08:50:33 -0800

    added delay of 1 second after change to high power
    It stopped climbing on the switch to 0.8 current spike. - Rob

--- src/org/usfirst/frc/team294/robot/commands/StartClimbSequence.java
+++ src/org/usfirst/frc/team294/robot/commands/StartClimbSequence.java
@@ -19,10 +19,11 @@ public class StartClimbSequence extends CommandGroup {
     	addSequential(new CheckClimberCurrent(10));
     	addSequential(new WaitSeconds(1));
     	addSequential(new ClimbSetToSpeed(0.7));
-    	addSequential(new WaitSeconds(2));
+    	addSequential(new WaitSeconds(1));
     	addSequential(new CheckClimberCurrent(16));
-    	addSequential(new ClimbSetToSpeed(.8));
-    	addSequential(new CheckClimberCurrent(38));
+    	addSequential(new ClimbSetToSpeed(.8));			//  test this.  If it works increase to 1.0
+    	addSequential(new WaitSeconds(1));				// Rob added after analyzing current graph from last night
+    	addSequential(new CheckClimberCurrent(30));		// We had increased this to 38 because it stopped here, but we didn't have delay - try 30 and measure
     	addSequential(new ClimbSetToSpeed(0));
     }
 }
