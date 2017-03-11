3d9bb3e8fcef27d48831c9eb9a58b75ae9e64b58
team <Team294@rbusd.org>
2017-03-11 13:36:48 -0800

    Revert "Added camera feed for gear vision"
    This reverts commit f7053b80fa2f0d8a606cbda94cf1e44d528a232c.

--- src/org/usfirst/frc/team294/robot/Robot.java
+++ src/org/usfirst/frc/team294/robot/Robot.java
@@ -1,9 +1,6 @@
 package org.usfirst.frc.team294.robot;
 
 import edu.wpi.first.wpilibj.CameraServer;
-import edu.wpi.cscore.CvSink;
-import edu.wpi.cscore.CvSource;
-import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
@@ -14,7 +11,6 @@ import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.Timer;
 
 import org.usfirst.frc.team294.robot.subsystems.*;
-import org.opencv.core.Mat;
 import org.usfirst.frc.team294.robot.commands.AutoGearAndScore;
 import org.usfirst.frc.team294.utilities.FileLog;
 
@@ -35,24 +31,24 @@ public class Robot extends IterativeRobot {
 	public static Intake intake;
 	public static Shooter shooter;
 	public static ShooterHood shooterHood;
-
+	
 	// Vision subsystems
 	public static BoilerVision boilerVision;
 	public static GearVision gearVision;
-
+	
 	// The OI
 	public static OI oi;
-
+	
 	// Turn on/off SmartDashboard debugging
 	public static boolean smartDashboardDebug = false;		// true to print lots of stuff on the SmartDashboard
-
+	
 	//Timer
 	public static Timer teleopTime;
 	public static double startTime;
-
+	
 	// File logger
 	public static FileLog log;
-
+	
 	// set up preferences (robot specific calibrations flash memory in roborio)
 	public static Preferences robotPrefs;
 	public static double shooterP;
@@ -78,13 +74,13 @@ public class Robot extends IterativeRobot {
 	 * used for any initialization code.
 	 */
 	public void robotInit() {
-
+		
 		System.out.println("Robot init");
 		log = new FileLog();	// Create log file first, so other code can use it
 		readPreferences();		// Read preferences next, so that subsystems can use the preference values.
-
+		
 		teleopTime = new Timer();
-
+						
 		// Create the subsytems
 		driveTrain = new DriveTrain();
 		shifter = new Shifter();
@@ -102,9 +98,9 @@ public class Robot extends IterativeRobot {
 		intake.intakeCurrentProtection();
 		driveTrain.leftCurrentProtection();
 		driveTrain.rightCurrentProtection();
-
+		
 		// Turn on drive camera
-		//		CameraServer.getInstance().startAutomaticCapture();
+//		CameraServer.getInstance().startAutomaticCapture();
 
 		// Create OI
 		oi = new OI();
@@ -118,128 +114,113 @@ public class Robot extends IterativeRobot {
 		SmartDashboard.putData(gearGate);
 		SmartDashboard.putData(shooterHood);
 		SmartDashboard.putData(ballFeed);
-		//		SmartDashboard.putData(gearVision);
-		//		SmartDashboard.putData(boilerVision);
-		new Thread(() -> {
-			UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-			camera.setResolution(320, 240);
-
-			CvSink cvSink = CameraServer.getInstance().getVideo();
-			CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
-
-			Mat source = new Mat();
-			Mat output = new Mat();
-
-			while(!Thread.interrupted()) {
-				cvSink.grabFrame(source);
-				outputStream.putFrame(output);
-			}
-		}).start();
+//		SmartDashboard.putData(gearVision);
+//		SmartDashboard.putData(boilerVision);
 	}
 
-/**
- * This function is called once each time the robot enters Disabled mode.
- * You can use it to reset any subsystem information you want to clear when
- * the robot is disabled.
- */
-public void disabledInit() {
-	intake.updateConflicts();
+	/**
+	 * This function is called once each time the robot enters Disabled mode.
+	 * You can use it to reset any subsystem information you want to clear when
+	 * the robot is disabled.
+	 */
+	public void disabledInit() {
+		intake.updateConflicts();
 
-}
-
-public void disabledPeriodic() {
-	Scheduler.getInstance().run();
-	//stops all motors
-	Robot.shooter.stop();
-	Robot.ballFeed.stop();
-	Robot.intake.stopIntake();
-	Robot.intake.stopClimber();
-	Robot.driveTrain.stop();
-
-	if (Robot.smartDashboardDebug) {
-		//SmartDashboard.putNumber("Gyro dubdubdubdubdubdubdubdubdub2", driveTrain.getGyroAngle());
-		SmartDashboard.putNumber("Gear Angle", Robot.gearVision.getGearAngleOffset());
-		SmartDashboard.putNumber("Gear Distance", Robot.gearVision.getGearDistance());
 	}
 
-	teleopTime.reset();
-}
+	public void disabledPeriodic() {
+		Scheduler.getInstance().run();
+		//stops all motors
+		Robot.shooter.stop();
+    	Robot.ballFeed.stop();
+    	Robot.intake.stopIntake();
+    	Robot.intake.stopClimber();
+    	Robot.driveTrain.stop();
+    	
+	    if (Robot.smartDashboardDebug) {
+	    	//SmartDashboard.putNumber("Gyro dubdubdubdubdubdubdubdubdub2", driveTrain.getGyroAngle());
+			SmartDashboard.putNumber("Gear Angle", Robot.gearVision.getGearAngleOffset());
+			SmartDashboard.putNumber("Gear Distance", Robot.gearVision.getGearDistance());
+	    }
 
-/**
- * This autonomous (along with the chooser code above) shows how to select
- * between different autonomous modes using the dashboard. The sendable
- * chooser code works with the Java SmartDashboard. If you prefer the
- * LabVIEW Dashboard, remove all of the chooser code and uncomment the
- * getString code to get the auto name from the text box below the Gyro
- *
- * You can add additional auto modes by adding additional commands to the
- * chooser code above (like the commented example) or additional comparisons
- * to the switch structure below with additional strings & commands.
- */
-
-public void autonomousInit() {
-	// schedule the autonomous command (example)
-	intake.updateConflicts();
-	log.writeLogEcho("Autonomous Mode Started");
-
-	autonomousCommand = new AutoGearAndScore(oi.readMiddleKnobTeam(), oi.readBottomKnobStartPosition());
-
-	if (autonomousCommand != null)
-		autonomousCommand.start();
-}
-
-/**
- * This function is called periodically during autonomous
- */
-public void autonomousPeriodic() {
-	Scheduler.getInstance().run();
-
-	//		driveTrain.logTalonStatus();
-}
-
-public void teleopInit() {
-	// This makes sure that the autonomous stops running when
-	// teleop starts running. If you want the autonomous to
-	// continue until interrupted by another command, remove
-	// this line or comment it out.
-	intake.updateConflicts();
-	log.writeLogEcho("Teleop Mode Started");
-	teleopTime.start();
-	startTime = teleopTime.get();
-	//SmartDashboard.putNumber("Gyro dubdubdubdubdubdubdubdubdub", driveTrain.getGyroAngle());
-
-	//DeployIntakeAndHopper();
-	//ShooterSetRPM(Robot.shootHighSpeed);
-}
-
-/**
- * This function is called periodically during operator control
- */
-public void teleopPeriodic() {
-	Scheduler.getInstance().run();	
-
-	Robot.shooter.updateSmartDashboardShooterSpeed();
-
-	if (Robot.smartDashboardDebug) {
-		//for testing purposes 
-		driveTrain.updateSmartDashboardEncoders();
-		boilerVision.updateSmartDashboard();
-		//driveTrain.logTalonStatus();
-
-		SmartDashboard.putNumber("Gear Angle", Robot.gearVision.getGearAngleOffset());
-		SmartDashboard.putNumber("Gear Distance", Robot.gearVision.getGearDistance());
-
-		shooter.updateSmartDashboard(); 
-		intake.updateSmartDashboard();
-		//			intake.logIntakeStatus();
+		teleopTime.reset();
 	}
 
-	shooter.periodicSetF();
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString code to get the auto name from the text box below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional commands to the
+	 * chooser code above (like the commented example) or additional comparisons
+	 * to the switch structure below with additional strings & commands.
+	 */
 
-	oi.readBottomKnobRaw();
-	oi.readMiddleKnobRaw();
+	public void autonomousInit() {
+		// schedule the autonomous command (example)
+		intake.updateConflicts();
+		log.writeLogEcho("Autonomous Mode Started");
+		
+		autonomousCommand = new AutoGearAndScore(oi.readMiddleKnobTeam(), oi.readBottomKnobStartPosition());
 
-	/*		if (false) {//(teleopTime.get() - startTime) >= 300) { //auto stops all non drive train motors after set time
+		if (autonomousCommand != null)
+			autonomousCommand.start();
+	}
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+		
+//		driveTrain.logTalonStatus();
+	}
+
+	public void teleopInit() {
+		// This makes sure that the autonomous stops running when
+		// teleop starts running. If you want the autonomous to
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
+		intake.updateConflicts();
+		log.writeLogEcho("Teleop Mode Started");
+		teleopTime.start();
+		startTime = teleopTime.get();
+		//SmartDashboard.putNumber("Gyro dubdubdubdubdubdubdubdubdub", driveTrain.getGyroAngle());
+		
+		//DeployIntakeAndHopper();
+		//ShooterSetRPM(Robot.shootHighSpeed);
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();	
+		
+		Robot.shooter.updateSmartDashboardShooterSpeed();
+		
+	    if (Robot.smartDashboardDebug) {
+		    //for testing purposes 
+			driveTrain.updateSmartDashboardEncoders();
+			boilerVision.updateSmartDashboard();
+			//driveTrain.logTalonStatus();
+
+			SmartDashboard.putNumber("Gear Angle", Robot.gearVision.getGearAngleOffset());
+			SmartDashboard.putNumber("Gear Distance", Robot.gearVision.getGearDistance());
+			
+			shooter.updateSmartDashboard(); 
+			intake.updateSmartDashboard();
+//			intake.logIntakeStatus();
+        }
+
+		shooter.periodicSetF();
+
+		oi.readBottomKnobRaw();
+		oi.readMiddleKnobRaw();
+
+/*		if (false) {//(teleopTime.get() - startTime) >= 300) { //auto stops all non drive train motors after set time
 			Robot.shooter.stop();
 	    	Robot.ballFeed.stop();
 	    	Robot.intake.stopIntake();
@@ -247,72 +228,72 @@ public void teleopPeriodic() {
 	    	System.out.println("All Motors Timed Out Reenable to Reset");
 
 	    }*/
-}
-
-/**
- * This function is called periodically during test mode
- */
-public void testPeriodic() {
-	LiveWindow.run();
-}
-
-/**
- * Read the preferences from the RoboRio flash memory.
- * For any missing preferences, set the preference to default values.
- */
-public void readPreferences() {
-	//TODO:  Create function to read and set defaults for one number preference, then move most prefs
-	//  to calling this function.  This will eliminate much of the duplicate code below.
-
-	//TODO:  For each robot preference:  Use more descriptive names?
-	robotPrefs = Preferences.getInstance();
-
-	if (robotPrefs.getDouble("inchesPerRev", 0) == 0) {
-		DriverStation.reportError("Error:  Preferences missing from RoboRio for Inches per Revolution calibration.", true);
-		robotPrefs.putDouble("inchesPerRev", 12.5); //this needs to be changed when we find the new value
 	}
-	inchesPerRevolution = robotPrefs.getDouble("inchesPerRev", 0);
 
-	shooterP = robotPrefs.getDouble("shooterP",0.15);// This has to be done before Shooter()
-	shooterI = robotPrefs.getDouble("shooterI",0);
-	shooterD = robotPrefs.getDouble("shooterD",0);
-	shooterFNominal = robotPrefs.getDouble("shooterFNominal",.024);
-
-	invertDrive = robotPrefs.getBoolean("invertDrive",true);
-
-	if(robotPrefs.getDouble("intakeSpeed",0) == 0){
-		robotPrefs.putDouble("intakeSpeed",1.0);
+	/**
+	 * This function is called periodically during test mode
+	 */
+	public void testPeriodic() {
+		LiveWindow.run();
 	}
-	intakeSpeed = robotPrefs.getDouble("intakeSpeed",0);
+	
+	/**
+	 * Read the preferences from the RoboRio flash memory.
+	 * For any missing preferences, set the preference to default values.
+	 */
+	public void readPreferences() {
+				//TODO:  Create function to read and set defaults for one number preference, then move most prefs
+				//  to calling this function.  This will eliminate much of the duplicate code below.
+				
+				//TODO:  For each robot preference:  Use more descriptive names?
+				robotPrefs = Preferences.getInstance();
+				
+				if (robotPrefs.getDouble("inchesPerRev", 0) == 0) {
+					DriverStation.reportError("Error:  Preferences missing from RoboRio for Inches per Revolution calibration.", true);
+					robotPrefs.putDouble("inchesPerRev", 12.5); //this needs to be changed when we find the new value
+				}
+				inchesPerRevolution = robotPrefs.getDouble("inchesPerRev", 0);
+				
+				shooterP = robotPrefs.getDouble("shooterP",0.15);// This has to be done before Shooter()
+				shooterI = robotPrefs.getDouble("shooterI",0);
+				shooterD = robotPrefs.getDouble("shooterD",0);
+				shooterFNominal = robotPrefs.getDouble("shooterFNominal",.024);
+				
+				invertDrive = robotPrefs.getBoolean("invertDrive",true);
+				
+				if(robotPrefs.getDouble("intakeSpeed",0) == 0){
+					robotPrefs.putDouble("intakeSpeed",1.0);
+				}
+				intakeSpeed = robotPrefs.getDouble("intakeSpeed",0);
 
+				
+				shootSpeedHighRPM = robotPrefs.getDouble("shootSpeedHighRPM",4200);
+				shootSpeedLowRPM = robotPrefs.getDouble("shootSpeedLowRPM",3800);
 
-	shootSpeedHighRPM = robotPrefs.getDouble("shootSpeedHighRPM",4200);
-	shootSpeedLowRPM = robotPrefs.getDouble("shootSpeedLowRPM",3800);
+				
+				if(robotPrefs.getDouble("horizontalConveyorInVolts",0) == 0){
+						robotPrefs.putDouble("horizontalConveyorInVolts", 9.0);
+					}	
+				horizontalConveyorInVolts = robotPrefs.getDouble("horizontalConveyorInVolts",0);
 
+				if(robotPrefs.getDouble("verticalConveyorInVolts",0) == 0){
+					robotPrefs.putDouble("verticalConveyorInVolts", 12.0);
+				}
+				verticalConveyorInVolts = robotPrefs.getDouble("verticalConveyorInVolts",0);
+				
+				if(robotPrefs.getDouble("horizontalConveyorOutVolts",0) == 0){
+					robotPrefs.putDouble("horizontalConveyorOutVolts", -9.0);
+				}
+				horizontalConveyorOutVolts = robotPrefs.getDouble("horizontalConveyorOutVolts", 0);
+				
+				if(robotPrefs.getDouble("verticalConveyorOutVolts",0) == 0){
+					robotPrefs.putDouble("verticalConveyorOutVolts", -9.0);
+				}
+				verticalConveyorOutVolts = robotPrefs.getDouble("verticalConveyorOutVolts",0);
+				
 
-	if(robotPrefs.getDouble("horizontalConveyorInVolts",0) == 0){
-		robotPrefs.putDouble("horizontalConveyorInVolts", 9.0);
-	}	
-	horizontalConveyorInVolts = robotPrefs.getDouble("horizontalConveyorInVolts",0);
+				gearCamHorizOffsetInches = robotPrefs.getDouble("gearCam",0);
 
-	if(robotPrefs.getDouble("verticalConveyorInVolts",0) == 0){
-		robotPrefs.putDouble("verticalConveyorInVolts", 7.5);
-	}
-	verticalConveyorInVolts = robotPrefs.getDouble("verticalConveyorInVolts",0);
-
-	if(robotPrefs.getDouble("horizontalConveyorOutVolts",0) == 0){
-		robotPrefs.putDouble("horizontalConveyorOutVolts", -2.0);
-	}
-	horizontalConveyorOutVolts = robotPrefs.getDouble("horizontalConveyorOutVolts", 0);
-
-	if(robotPrefs.getDouble("verticalConveyorOutVolts",0) == 0){
-		robotPrefs.putDouble("verticalConveyorOutVolts", -2.0);
-	}
-	verticalConveyorOutVolts = robotPrefs.getDouble("verticalConveyorOutVolts",0);
-
-
-	gearCamHorizOffsetInches = robotPrefs.getDouble("gearCam",0);
-
-}	
+			}	
 
 }
